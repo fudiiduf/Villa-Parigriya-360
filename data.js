ec737a90099b2af54c04bbc87ce0cce190f142d9
fudiiduf <130423736+fudiiduf@users.noreply.github.com>
2023-08-27 10:03:26 +0700

    Commit

---
+++ data.js
@@ -0,0 +1,415 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ruang-tamu--dapur",
+      "name": "Ruang Tamu & Dapur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16203594471113192,
+          "pitch": -0.02333389451398382,
+          "rotation": 0,
+          "target": "3-ruang-tidur-utama"
+        },
+        {
+          "yaw": -0.4605624512642361,
+          "pitch": -0.020389016580301345,
+          "rotation": 4.71238898038469,
+          "target": "2-kolam-renang"
+        },
+        {
+          "yaw": 0.2820056389942742,
+          "pitch": -0.03638262006194992,
+          "rotation": 0,
+          "target": "4-ruang-keluarga-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.021137602239129194,
+          "pitch": -0.03222617058328048,
+          "title": "Kamar Mandi 1",
+          "text": "Kamar mandi untuk tamu atau keluarga, aksesnya yang dekat dengan Kolam Renang juga memudahkan ketika selesai beraktifitas di Kolam Renang."
+        },
+        {
+          "yaw": -1.569016070253797,
+          "pitch": -0.20142412454227632,
+          "title": "Kitchen Cabinet Built In",
+          "text": "Kitchen Cabinet dan Meja Makan built in dengan desain modern. Desain yang cermat tidak hanya membuat dapur semakin terlihat estetik, tapi juga memastikan semua peralatan dapur serta bahan makanan bisa ter-organisir dengan baik dan rapi."
+        },
+        {
+          "yaw": 2.7974520914517838,
+          "pitch": -0.024178738405620237,
+          "title": "Partisi Kondisional",
+          "text": "Berfungsi sebagai sekat ruang yang fleksibel antara Ruang Tamu dan Ruang Keluarga, sekat bisa di fungsikan ketika membutuhkan privasi, ketika tidak di fungsikan akan membuat ruangan menjadi semakin luas dan terbuka, sementara sekat bisa menjadi aksen ruangan yang membuat ruangan semakin tampak estetik."
+        },
+        {
+          "yaw": 1.232577858053764,
+          "pitch": 0.005124215981295421,
+          "title": "Foyer",
+          "text": "Area transisi tamu sebelum masuk ke area Ruang Tamu. View Patio / Taman Indoor akan langsung menyambut tamu ketika masuk di area Foyer."
+        },
+        {
+          "yaw": -1.732932347952456,
+          "pitch": 0.24266816769739386,
+          "title": "Dining Island Built In",
+          "text": "Meja Makan built in dengan desain modern, di desain dengan konsep multifungsi yang bisa berguna sebagai meja makan atau juga sebagai meja pantry."
+        }
+      ]
+    },
+    {
+      "id": "1-ruang-keluarga-1",
+      "name": "Ruang Keluarga 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.5068754803327096,
+        "pitch": 0.011339428818272879,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.100824397183768,
+          "pitch": -0.03301978867449762,
+          "rotation": 1.5707963267948966,
+          "target": "3-ruang-tidur-utama"
+        },
+        {
+          "yaw": 0.3118546691839619,
+          "pitch": -0.027644552407016576,
+          "rotation": 0,
+          "target": "2-kolam-renang"
+        },
+        {
+          "yaw": 1.9736073254822104,
+          "pitch": -0.010672936389779508,
+          "rotation": 0,
+          "target": "4-ruang-keluarga-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0983658874505124,
+          "pitch": 0.07725636034322214,
+          "title": "Ruang Tidur Utama",
+          "text": "Ruang tidur utama seluas 12 m2 dengan fitur kamar mandi dalam seluas 6 m2 yang dilengkapi dengan shower dan bathtub dan view langsung ke arah kolam renang."
+        },
+        {
+          "yaw": 0.3099168804432537,
+          "pitch": 0.09640720042049544,
+          "title": "Kolam Renang",
+          "text": "View Kolam Renang langsung di hadapan mata, dapat di akses dengan mudah dari Ruang Keluarga&nbsp; atau dari Pantry. Vertical Garden sebagai latar belakang Kolam Renang menghadirkan suasana alami di dalam rumah."
+        },
+        {
+          "yaw": -1.5708537761002042,
+          "pitch": 0.028382626649859688,
+          "title": "Partisi Kondisional",
+          "text": "Berfungsi sebagai sekat ruang yang fleksibel antara Ruang Tamu dan Ruang Keluarga, sekat bisa di fungsikan ketika membutuhkan privasi, ketika tidak di fungsikan akan membuat ruangan menjadi semakin luas dan terbuka, sementara sekat bisa menjadi aksen ruangan yang membuat ruangan semakin estetik."
+        },
+        {
+          "yaw": -1.0808427739914563,
+          "pitch": 0.05019749163767173,
+          "title": "Dapur &amp; Ruang Makan",
+          "text": "Desain Dapur berkonsep bersih terbuka dengan Meja Island yang multifungsi bisa sebagai meja makan atau meja pantry. Kitchen Cabinet dan Meja Makan built in dengan desain modern."
+        },
+        {
+          "yaw": 1.4094580245511263,
+          "pitch": 0.07437173632060201,
+          "title": "Kamar Mandi 1",
+          "text": "Kamar mandi untuk tamu atau keluarga, aksesnya yang dekat dengan Kolam Renang juga memudahkan ketika selesai beraktifitas di Kolam Renang."
+        },
+        {
+          "yaw": -3.0181702770377186,
+          "pitch": -0.04527945459794225,
+          "title": "Patio",
+          "text": "Patio / Taman Indoor, selain sebagai area sirkulasi pencahayaan dan udara alami, juga bisa&nbsp; menghadirkan suasana relaksasi&nbsp;alami di dalam rumah.<div>View Patio akan langsung menyambut tamu ketika masuk di area Foyer.</div>"
+        },
+        {
+          "yaw": -1.9129588729697389,
+          "pitch": 0.025107965943302446,
+          "title": "Ruang Tamu",
+          "text": "Area Ruang Tamu yang terpisah memungkinkan privasi keluarga tetap terjaga, akan tetapi juga bisa di alih fungsikan menjadi ruang multifungsi dengan konsep open space."
+        }
+      ]
+    },
+    {
+      "id": "2-kolam-renang",
+      "name": "Kolam Renang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.339243757967317,
+          "pitch": 0.06987381521722114,
+          "rotation": 1.5707963267948966,
+          "target": "3-ruang-tidur-utama"
+        },
+        {
+          "yaw": 0.4833974236898779,
+          "pitch": 0.034365103018361154,
+          "rotation": 1.5707963267948966,
+          "target": "1-ruang-keluarga-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08911496326185997,
+          "pitch": 0.02868203060553043,
+          "title": "Kamar Mandi 1",
+          "text": "Kamar mandi untuk tamu atau keluarga, aksesnya yang dekat dengan Kolam Renang juga memudahkan ketika selesai beraktifitas di Kolam Renang."
+        },
+        {
+          "yaw": 1.263175270408997,
+          "pitch": 0.047252423641756636,
+          "title": "Dapur &amp; Ruang Makan",
+          "text": "Dengan view langsung ke arah kolam renang kegiatan di Dapur atau Ruang Makan jadi tidak membosankan, akses langsung ke Dek Kolam Renang dan Ruang Keluarga juga membuat aktifitas semakin&nbsp;&nbsp;nyaman."
+        },
+        {
+          "yaw": -1.4123022440543735,
+          "pitch": -0.9761798950595537,
+          "title": "Ruang Keluarga 2",
+          "text": "Pemandangan Kolam Renang bisa langsung dinikmati dari dalam Ruang Keluarga di lantai 2."
+        },
+        {
+          "yaw": 1.4426821265280907,
+          "pitch": -0.7991873917276138,
+          "title": "Ruang Tidur 1",
+          "text": "Pemandangan Kolam Renang bisa langsung dinikmati dari dalam kamar tidur di lantai 2."
+        },
+        {
+          "yaw": 2.6410960951733715,
+          "pitch": -0.038449489855604924,
+          "title": "Vertical Garden",
+          "text": "Desain Vertical Garden pada dinding belakang menghadirkan nuansa alami dari dalam rumah, juga semakin menambah suasana relaksasi ketika beraktifitas di kolam renang."
+        },
+        {
+          "yaw": -1.3370176078190266,
+          "pitch": 0.1847399860230201,
+          "title": "Ruang Tidur Utama",
+          "text": "Pemandangan Kolam Renang bisa langsung dinikmati dari dalam kamar tidur utama."
+        },
+        {
+          "yaw": -1.8524214529271497,
+          "pitch": 0.14110460923290447,
+          "title": "Title",
+          "text": "Pemandangan Kolam Renang bisa langsung dinikmati dari dalam Kamar Mandi Utama sambil berendam santai di Bathtub."
+        },
+        {
+          "yaw": -1.889236035289482,
+          "pitch": -0.6625266716148221,
+          "title": "Ruang Cuci &amp; Jemur",
+          "text": "Dengan konsep&nbsp;dan desain&nbsp;Lay Out yang cermat, penempatan Ruang Cuci &amp; Jemur tidak akan mengganggu estetika dan pandangan dari dalam atau luar rumah. Rumah akan selalu terlihat rapi dan estetik."
+        }
+      ]
+    },
+    {
+      "id": "3-ruang-tidur-utama",
+      "name": "Ruang Tidur Utama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.03668653154574386,
+          "pitch": 0.03529459381701017,
+          "title": "Master Bath Room",
+          "text": "Kamar Mandi Utama seluas 6 m2 dengan fitur Bathtub, Shower, Kloset duduk, dan Vanity Mirror yang terpisah, dengan konsep&nbsp;&nbsp;desain yang clean, minimalis, serta modern. View kolam renang langsung di depan bathtub mendukung suasana relaksasi ketika berendam, juga memberikan pencahayaan natural di siang hari sehingga kamar mandi tetap terang, kering, dan tidak lembab.&nbsp;"
+        },
+        {
+          "yaw": -1.0090334728560748,
+          "pitch": 0.038225225749371816,
+          "title": "Kolam Renang &amp; Vertical Garden",
+          "text": "View kolam renang dan Vertical Garden bisa langsung di nikmati leluasa langsung dari tempat tidur tanpa harus keluar ruangan."
+        },
+        {
+          "yaw": -2.2527964814554835,
+          "pitch": 0.06564273795211051,
+          "title": "Ruang Keluarga 1",
+          "text": "Akses langsung ke ruang keluarga membuat suasana rumah menjadi semakin intim bersama keluarga."
+        },
+        {
+          "yaw": 0.5487628148027692,
+          "pitch": 0.018334604929075127,
+          "title": "Lemari Built in",
+          "text": "Desain ranjang &amp; lemari wardrobe built in, di konsep Fit in dalam ruangan untuk memastikan semua barang atau pakaian tertata rapi tanpa tercecer, posisi serta peletakanya di desain secara matang sehingga tidak mengganggu sirkulasi, baik pencahayaan, udara ataupun ruang gerak."
+        }
+      ]
+    },
+    {
+      "id": "4-ruang-keluarga-2",
+      "name": "Ruang Keluarga 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9889449008524007,
+          "pitch": 0.2817520564607747,
+          "rotation": 3.141592653589793,
+          "target": "1-ruang-keluarga-1"
+        },
+        {
+          "yaw": -0.3741888650682643,
+          "pitch": 0.3623138660594236,
+          "rotation": 3.141592653589793,
+          "target": "2-kolam-renang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4681562679988325,
+          "pitch": 0.016959359307650246,
+          "title": "Ruang Cuci &amp; Jemur",
+          "text": "Ruang Laundry &amp; Jemur yang terpisah dari area utama, dengan sirkulasi pencahayaan serta udara yang natural memastikan cucian kering sempurna tanpa lembab, Ruang jemur di desain agar tidak mengganggu pandangan yang tampak dari luar atau dari sisi manapun di dalam rumah."
+        },
+        {
+          "yaw": -1.5198435801151469,
+          "pitch": -0.009931547722889178,
+          "title": "Ruang Tidur 1",
+          "text": "Kamar tidur dengan luas 10 m2 dengan fitur furnitur lemari &amp; ranjang built in, memiliki view ke arah kolam renang dan kaki gunung Panderman."
+        },
+        {
+          "yaw": -1.7767940395047113,
+          "pitch": -0.012556955140809123,
+          "title": "Kamar Mandi 2",
+          "text": "Kamar Mandi luas 2,4 m2 dengan fitur shower, kloset duduk dan vanity mirror dengan konsep desain minimalis modern dengan sentuhan nuansa tropis pada finishing furniturnya."
+        },
+        {
+          "yaw": -1.889537616972163,
+          "pitch": -0.012117475256038901,
+          "title": "Ruang Tidur 2",
+          "text": "Kamar tidur dengan luas 10 m2 dengan fitur furnitur lemari &amp; ranjang built in, memiliki view ke arah taman depan, di lengkapi dengan balkon seluas 3,5M2 yang menghadap langsung ke Canopy Garden di atas Car Port."
+        },
+        {
+          "yaw": -2.694692872325337,
+          "pitch": 0.09469857794138647,
+          "title": "Canopy Garden &amp; Balcony",
+          "text": "Ruang terbuka dengan konsep taman yang multifungsi, bisa di gunakan sebagai area santai atau area tambahan untuk kegiatan bersama keluarga besar dengan view ke arah kebun jeruk dan kota batu di bawah."
+        },
+        {
+          "yaw": -0.37301244243796283,
+          "pitch": 0.06462794500034796,
+          "title": "View Panorama",
+          "text": "View ke arah kolam renang dan kaki gunung Panderman di latar belakang"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
